eb230f6117f34920c8ae0bbd968bd2354337f927
Your Name <selimserifoglu03@gmail.com>
2021-06-02 17:48:36 +0300

    none

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,26 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Button = styled.button`
-//           display: none;
-//           background: ${props => {
-//               if(props.green){
-//                   return "#4FAE16";
-//               }else if(props.red){
-//                   return "#ff050d";
-//               }else{
-//                   return "#F2DA00";
-//               }
-//           }};
-//           border-radius: 5px;
-//           color: #353535;
-//           font-size: 22px;
-//           font-weight: bold;
-//           border: none;
-//           width: 80%;
-//           margin: .5rem auto;
-//           padding: 0.5rem 0;
-//           &:hover{
-//               opacity: .8;
-//           }
+export const Button = styled.button`
+          display: none;
+          background: ${props => {
+              if(props.green){
+                  return "#4FAE16";
+              }else if(props.red){
+                  return "#ff050d";
+              }else{
+                  return "#F2DA00";
+              }
+          }};
+          border-radius: 5px;
+          color: #353535;
+          font-size: 22px;
+          font-weight: bold;
+          border: none;
+          width: 80%;
+          margin: .5rem auto;
+          padding: 0.5rem 0;
+          &:hover{
+              opacity: .8;
+          }
 
-// `;
+`;
